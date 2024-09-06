f8f368c663f74bcd94d9098bdb3ca6d7eb262351
Egor <egorboom2017@gmail.com>
2024-09-06 12:16:23 +0300

    fix burger menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,18 @@
 import './style.scss';
 
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 import logo from '@/assets/logo.svg';
 
+import { links } from '../../constants';
 import { useMobileView } from '../../utils/hooks';
+import { BurgerMenu } from '../BurgerMenu/BurgerMenu.tsx';
 
 export const Header: FC = () => {
   const location = useLocation();
   const isMobile = useMobileView();
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const links = [
-    {
-      name: 'Главная',
-      path: '/',
-    },
-    {
-      name: 'Избранное',
-      path: '/Favorites',
-    },
-  ];
-
+  
   return (
     <header className="header">
       <div className="header-logo">
@@ -31,25 +21,7 @@ export const Header: FC = () => {
         </Link>
       </div>
       {isMobile ? (
-        <>
-          <div className={`burger-menu ${menuOpen ? 'open' : ''}`} onClick={() => setMenuOpen(!menuOpen)}>
-            <div className="burger-bar"></div>
-            <div className="burger-bar"></div>
-            <div className="burger-bar"></div>
-          </div>
-          <div className={`mobile-menu ${menuOpen ? 'open' : ''}`}>
-            {links.map((link) => (
-              <Link
-                className={`${location.pathname === link.path ? 'active' : ''}`}
-                to={link.path}
-                key={link.path}
-                onClick={() => setMenuOpen(false)}
-              >
-                {link.name}
-              </Link>
-            ))}
-          </div>
-        </>
+        <BurgerMenu />
       ) : (
         <div className="header-links">
           {links.map((link) => (
